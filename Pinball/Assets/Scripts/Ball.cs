21053374acf2a8a6d1c3ca8af3c88b338198e1c4
larachille <clara@familie-hille.de>
2018-12-01 14:51:23 +0100

    Added board and ball

---
+++ Pinball/Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	[SerializeField]
+	float speed;
+
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce (0,0,speed, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
