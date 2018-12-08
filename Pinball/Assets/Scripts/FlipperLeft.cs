653955139af0a227ac6fe5b3d7cfe588604bcbd6
larachille <clara@familie-hille.de>
2018-12-08 18:08:39 +0100

    Made Flippers work

---
+++ Pinball/Assets/Scripts/FlipperLeft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperLeft : MonoBehaviour {
+
+	Quaternion origRotation; 
+	Quaternion flipRotation;
+
+	// Use this for initialization
+	void Start () {
+		origRotation = transform.rotation;
+		flipRotation = Quaternion.Euler (0f, -36.5f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("a")){
+			transform.rotation = flipRotation;
+		}
+		if (Input.GetKeyUp("a")) {
+			transform.rotation = origRotation;
+		}
+	}
+}
---
+++ Pinball/Assets/Scripts/FlipperRight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperRight : MonoBehaviour {
+
+	Quaternion origRotation; 
+	Quaternion flipRotation;
+
+	// Use this for initialization
+	void Start () {
+		origRotation = transform.rotation;
+		flipRotation = Quaternion.Euler (0f, 36.5f, 0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("d")){
+			transform.rotation = flipRotation;
+		}
+		if (Input.GetKeyUp("d")) {
+			transform.rotation = origRotation;
+		}
+	}
+}
